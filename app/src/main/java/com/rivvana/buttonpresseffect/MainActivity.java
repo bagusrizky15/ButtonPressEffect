9869068be9592c9fab6cb03d4108310a238ab8f9
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-13 20:58:18 +0700

    add imgv

--- app/src/main/java/com/rivvana/buttonpresseffect/MainActivity.java
+++ app/src/main/java/com/rivvana/buttonpresseffect/MainActivity.java
@@ -2,13 +2,19 @@ package com.rivvana.buttonpresseffect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button imageButton;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addListenerOnButton();
+
+        imageView = findViewById(R.id.ivButton);
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        ImageView view = (ImageView) v;
+                        view.getDrawable().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL: {
+                        ImageView view = (ImageView) v;
+                        view.getDrawable().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+
+                }
+
+                return false;
+            }
+        });
     }
 
     private void addListenerOnButton() {
