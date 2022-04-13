1115738ce16cf77291583e81ee27236b84457880
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-13 20:13:11 +0700

    added color

--- app/src/main/java/com/rivvana/buttonpresseffect/MainActivity.java
+++ app/src/main/java/com/rivvana/buttonpresseffect/MainActivity.java
@@ -3,12 +3,26 @@ package com.rivvana.buttonpresseffect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    private void addListenerOnButton() {
+        imageButton = (Button) findViewById(R.id.imageButtonSelector);
+        imageButton.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Image was Selected", Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
